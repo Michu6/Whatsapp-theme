ab50c25048aafcdd00e9b0fdc28fc83710cb55ae
Michu6 <karenmvaldivia@gmail.com>
2016-06-07 23:12:45 -0500

    agregando funcionalidad al wsp

---
+++ js/data.js
@@ -0,0 +1,18 @@
+var contactos = [
+	{
+		"usser": "Laboratoria Perú",
+		"img": "logocodeacademy.png",
+	},
+	{
+		"usser": "Raymi Saldomando",
+		"img": "raymi.jpg",
+	},
+	{
+		"usser": "Mariana Costa",
+		"img": "mariana.jpg",
+	},
+	{
+		"usser": "Karen Valdivia",
+		"img": "karen.jpg",
+	}
+]
